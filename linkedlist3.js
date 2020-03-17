94a94f980642867440b9584cf95711e85565dc25
jsteinlein <51655187+jsteinlein@users.noreply.github.com>
2020-03-16 20:33:43 -0700

    Add files via upload

---
+++ linkedlist3.js
@@ -0,0 +1,28 @@
+function ListNode(value) {
+    this.val = value;
+    this.next = null;
+}
+
+back() {
+    var runner = this.head;
+    while(runner.next) {
+        runner = runner.next
+    }
+    return runner.value
+}
+removeBack() {
+    var runner = this.head;
+    while(runner.next.next) {
+        runner = runner.next
+    }
+    runner.next = null;
+    return this
+}
+addBack(value) {
+    var runner = this.head;
+    while(runner.next) {
+        runner = runner.next
+    }
+    runner.next = new Node(value);
+    return this
+}
